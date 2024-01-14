4e93f2fafb643a885360b6b3edddef5ba7baaba1
Marten de Jong <87567797+mg-de-j@users.noreply.github.com>
2024-01-14 11:56:45 +0100

    Fix GraphQL queries and remove commented code. Resolved issue "cat infinite loading"

--- lib/src/graphql/fragments/__generated__/subForm_processing.ast.gql.dart
+++ lib/src/graphql/fragments/__generated__/subForm_processing.ast.gql.dart
@@ -17,6 +17,13 @@ const SubFormProcessing = _i1.FragmentDefinitionNode(
   )),
   directives: [],
   selectionSet: _i1.SelectionSetNode(selections: [
+    _i1.FieldNode(
+      name: _i1.NameNode(value: '__typename'),
+      alias: null,
+      arguments: [],
+      directives: [],
+      selectionSet: null,
+    ),
     _i1.InlineFragmentNode(
       typeCondition: _i1.TypeConditionNode(
           on: _i1.NamedTypeNode(
--- lib/src/result/queries/get_sub_forms.dart
+++ lib/src/result/queries/get_sub_forms.dart
@@ -4,7 +4,8 @@ import 'package:get_it/get_it.dart';
 
 import '../../../__generated__/schema.schema.gql.dart';
 import '../../graphql/__generated__/details_subforms.req.gql.dart';
-import '../../graphql/__generated__/search_lemma_subforms.req.gql.dart';
+import '../../graphql/__generated__/search_lemma.req.gql.dart';
+// import '../../graphql/__generated__/search_lemma_subforms.req.gql.dart';
 
 Future getSubForms() async {
   final client = GetIt.I<TypedLink>();
@@ -14,7 +15,7 @@ Future getSubForms() async {
       ..vars.lemma = varController.query
       ..vars.source = "fkw");
   } else {
-    request = GSearchLemmaSubFormReq((b) => b
+    request = GsearchLemmaReq((b) => b
       ..vars.searchterm = varController.query
       ..vars.lang = GLangType.en
       ..vars.offset = 0
@@ -25,54 +26,50 @@ Future getSubForms() async {
 
   final response = await client.request(request).first;
 
+  var data = response.data;
+
   if (response.graphqlErrors != null || response.hasErrors) {
     return [];
   }
 
-  var data = response.data;
+  
 
   final subForms = [];
 
-  var lemmas = varController.isFryEn ? data.details : data.lemmasearch?.lemmas;
+  var lemmas = varController.isFryEn ? data.details : data.lemmasearch;
+
+  if(!varController.isFryEn) lemmas = lemmas.lemmas;
 
   if (lemmas == null || lemmas.isEmpty) return [];
 
-  for (var item in lemmas) {
-    if (item.lemma.form == varController.query) {
-      if (item.lemma.subForms != null) {
-        for (var subForm in item.lemma.subForms!) {
-          subForms.add(subForm);
+
+  if (varController.isFryEn) {
+    for (var item in lemmas) {
+      if (item.lemma.form == varController.query) {
+        if (item.lemma.subForms != null) {
+          for (var subForm in item.lemma.subForms!) {
+            subForms.add(subForm);
+          }
         }
+        return subForms.toSet().toList();
       }
-      return subForms.toSet().toList();
+    }
+  } else {
+    for (var item in lemmas) {
+      if (item.form == varController.query) {
+        if (item.subForms != null) {
+          for (var subForm in item.subForms!) {
+            subForms.add(subForm);
+          }
+        }
+        return subForms.toSet().toList();
+      }
     }
   }
-
+  
   for (var subform in lemmas.first.lemma.subForms) {
     subForms.add(subform);
   }
 
   return subForms.toSet().toList();
-
-  // if (!varController.isFryEn) {
-  //   final lemmas = response.data?.lemmasearch?.lemmas;
-
-  //   for (var lemma in lemmas!) {
-  //     if (lemma?.form != varController.query) {
-  //       continue;
-  //     }
-
-  //     if (lemma?.subForms != null) {
-  //       for (var subForm in lemma!.subForms!) {
-  //         subForms.add(subForm);
-  //       }
-  //     }
-  //   }
-  // } else {
-  //   for (var subForm in data.details.first.lemma.subForms) {
-  //     subForms.add(subForm);
-  //   }
-  // }
-
-  // return subForms.toSet().toList();
 }
