a8362c156b33cc75af1b4131ea8c0f2360643465
eduarddrenth <eduarddrenth@gmail.com>
2024-09-27 16:47:38 +0200

    comment

--- lib/client.dart
+++ lib/client.dart
@@ -3,6 +3,8 @@ import 'package:hive_flutter/hive_flutter.dart';
 
 Future<GraphQLClient> initClient() async {
   final HttpLink httpLink = HttpLink(
+    // TODO for now replace url with correct one and don't check in
+    // see https://github.com/flutter/flutter/issues/139289
     const String.fromEnvironment("api", defaultValue: "http://localhost:8080/servlet/graphql"),
   );
 
