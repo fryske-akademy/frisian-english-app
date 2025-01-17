346a4f2900d7eb19de1d0c8324637959a92213e2
eduarddrenth <eduarddrenth@gmail.com>
2025-01-17 09:37:26 +0100

    updates, analyze

--- lib/src/text_search/text_result.dart
+++ lib/src/text_search/text_result.dart
@@ -75,7 +75,7 @@ class _TextResultState extends State<TextResult> {
           return Text('Error: ${snapshot.error}');
         } else if (snapshot.data.length==0) {
           return const Material(
-              child: Padding(padding: const EdgeInsets.only(top: 50),
+              child: Padding(padding: EdgeInsets.only(top: 50),
               child: Center(
                   child: Column(
                       children: [Text("Nothing found"),BackButton()]
